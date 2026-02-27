c0886abaa544356bf838c2ff94eeb59be4e87e08
Marco Figueroa <mfigueroa@wunderbar.internal>
2026-02-27 13:25:15 -0800

    Configuración: variables de entorno para producción y desarrollo

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/google-script': {
-        target: 'https://script.google.com/macros/s/AKfycbyzq1D-H7v9IT3necFTTgUhGJ3bGqOB1G5f342Rh4Jey4mL6RFkBauYjdVBmyrjgz2Z/exec',
+        target: 'https://script.google.com/macros/s/AKfycbyzZuYw3IBC6w8-02-zMlU6QB3lImGtiWslPX86vn_Yi6pLJfMmdRMMq9Z4MWt-QrLn/exec',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/google-script/, '/exec'),
         configure: (proxy) => {
